9960b2525fadb1dc6c0de0e10d68f5cac54aa137
Enrico Wegner <wegner1996@gmail.com>
2025-01-30 14:25:48 +0100

    adding through and notThrough helper functions

---
+++ through.m
@@ -0,0 +1,14 @@
+function q = through(idx, horizons, order)
+    if length(idx) > 1 && iscell(horizons)
+        % Case when idx is a vector
+        qs = arrayfun(@(ii) through(idx(ii), horizons{ii}, order), 1:length(idx), 'UniformOutput', false);
+        q = qs{1};
+        for i = 2:length(qs)
+            q = q & qs{i};
+        end
+    else
+        % Case when idx is a scalar
+        s = strjoin(arrayfun(@(h) sprintf('y_{%d, %d}', idx, h), horizons, 'UniformOutput', false), ' & ');
+        q = makeConditionY(s, order);
+    end
+end
