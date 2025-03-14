1014f8a3c606a6710fbc4a31f9ea8c8053f68416
Enrico Wegner <wegner1996@gmail.com>
2025-03-14 16:10:57 +0100

    separating getShockSize from getShockIdx

--- dynare/getShockSize.m
+++ dynare/getShockSize.m
@@ -1,20 +1,17 @@
-function [shockSize, idx] = getShockSize(M_, shockName)
-    % `getShockSize` Obtain the standard deviation and index of a specified shock.
+function shockSize = getShockSize(shockName, M_)
+    % `getShockSize` Obtain the standard deviation of a specified shock.
     %
-    %   `[shockSize, idx] = getShockSize(M_, shockName)` computes the standard
+    %   `shockSize = getShockSize(shockName, M_)` computes the standard
     %   deviation (size) of a specified shock in a DSGE model estimated using Dynare.
-    %   It also returns the index of the shock within the internal shock vector.
     %
     %   ## Arguments
-    %   - `M_` (struct): Returned by Dynare.
     %   - `shockName` (string): The name of the shock whose size and index are required.
+    %   - `M_` (struct): Returned by Dynare.
     %
     %   ## Returns
     %   - `shockSize` (double): The standard deviation of the specified shock.
-    %   - `idx` (integer): The index of the shock in the internal shock vector.
     %
 
-    shocks = dynareCellArrayToVec(M_.exo_names);
-    idx = find(shocks == shockName);
+    idx = getShockIdx(shockName, M_);
     shockSize = sqrt(M_.Sigma_e(idx, idx));
 end
--- tests/dynareTest.m
+++ tests/dynareTest.m
@@ -27,43 +27,50 @@ function testDynareVarmaSW2007(testCase)
 
     irfsDSGEea = [dy_ea dc_ea dinve_ea pinfobs_ea dw_ea robs_ea labobs_ea];
     irfsDSGEea = reshape(irfsDSGEea', n, 1, []);
-    [shockSize, idx] = getShockSize(M_, "ea");  % only need this for idx 
+    shockSize = getShockSize("ea", M_);
+    idx = getShockIdx("ea", M_);
     testea = max(vec(abs(irfsDSGEea - irfsVarma(:, idx, :))));
     assert(testea < tol);
 
     irfsDSGEeb = [dy_eb dc_eb dinve_eb pinfobs_eb dw_eb robs_eb labobs_eb];
     irfsDSGEeb = reshape(irfsDSGEeb', n, 1, []);
-    [shock_size, idx] = getShockSize(M_, "eb");
+    shockSize = getShockSize("eb", M_);
+    idx = getShockIdx("eb", M_);
     testeb = max(vec(abs(irfsDSGEeb - irfsVarma(:, idx, :))));
     assert(testeb < tol);
 
     irfsDSGEeg = [dy_eg dc_eg dinve_eg pinfobs_eg dw_eg robs_eg labobs_eg];
     irfsDSGEeg = reshape(irfsDSGEeg', n, 1, []);
-    [shock_size, idx] = getShockSize(M_, "eg");
+    shockSize = getShockSize("eg", M_);
+    idx = getShockIdx("eg", M_);
     testeg = max(vec(abs(irfsDSGEeg - irfsVarma(:, idx, :))));
     assert(testeg < tol);
 
     irfsDSGEeqs = [dy_eqs dc_eqs dinve_eqs pinfobs_eqs dw_eqs robs_eqs labobs_eqs];
     irfsDSGEeqs = reshape(irfsDSGEeqs', n, 1, []);
-    [shock_size, idx] = getShockSize(M_, "eqs");
+    shockSize = getShockSize("eqs", M_);
+    idx = getShockIdx("eqs", M_);
     testeqs = max(vec(abs(irfsDSGEeqs - irfsVarma(:, idx, :))));
     assert(testeqs < tol);
 
     irfsDSGEem = [dy_em dc_em dinve_em pinfobs_em dw_em robs_em labobs_em];
     irfsDSGEem = reshape(irfsDSGEem', n, 1, []);
-    [shock_size, idx] = getShockSize(M_, "em");
+    shockSize = getShockSize("em", M_);
+    idx = getShockIdx("em", M_);
     testem = max(vec(abs(irfsDSGEem - irfsVarma(:, idx, :))));
     assert(testem < tol);
 
     irfsDSGEepinf = [dy_epinf dc_epinf dinve_epinf pinfobs_epinf dw_epinf robs_epinf labobs_epinf];
     irfsDSGEepinf = reshape(irfsDSGEepinf', n, 1, []);
-    [shock_size, idx] = getShockSize(M_, "epinf");
+    shockSize = getShockSize("epinf", M_);
+    idx = getShockIdx("epinf", M_);
     testepinf = max(vec(abs(irfsDSGEepinf - irfsVarma(:, idx, :))));
     assert(testepinf < tol);
 
     irfsDSGEew = [dy_ew dc_ew dinve_ew pinfobs_ew dw_ew robs_ew labobs_ew];
     irfsDSGEew = reshape(irfsDSGEew', n, 1, []);
-    [shock_size, idx] = getShockSize(M_, "ew");
+    shockSize = getShockSize("ew", M_);
+    idx = getShockIdx("ew", M_);
     testew = max(vec(abs(irfsDSGEew - irfsVarma(:, idx, :))));
     assert(testew < tol);
 end
@@ -93,19 +100,22 @@ function testDynareVarmaGali2015(testCase)
 
     irfsDSGEeps_nu = [pi_eps_nu, i_eps_nu, y_gap_eps_nu];
     irfsDSGEeps_nu = reshape(irfsDSGEeps_nu', n, 1, []);
-    [shockSize, idx] = getShockSize(M_, "eps_nu");  % only need this for idx 
+    shockSize = getShockSize("eps_nu", M_);
+    idx = getShockIdx("eps_nu", M_);
     testeps_nu = max(vec(abs(irfsDSGEeps_nu - irfsVarma(:, idx, :))));
     assert(testeps_nu < tol);
 
     irfsDSGEeps_z = [pi_eps_z, i_eps_z, y_gap_eps_z];
     irfsDSGEeps_z = reshape(irfsDSGEeps_z', n, 1, []);
-    [shockSize, idx] = getShockSize(M_, "eps_z");  % only need this for idx 
+    shockSize = getShockSize("eps_z", M_);
+    idx = getShockIdx("eps_z", M_);
     testeps_z = max(vec(abs(irfsDSGEeps_z - irfsVarma(:, idx, :))));
     assert(testeps_z < tol);
 
     irfsDSGEeps_a = [pi_eps_a, i_eps_a, y_gap_eps_a];
     irfsDSGEeps_a = reshape(irfsDSGEeps_a', n, 1, []);
-    [shockSize, idx] = getShockSize(M_, "eps_a");  % only need this for idx 
+    shockSize = getShockSize("eps_a", M_);
+    idx = getShockIdx("eps_a", M_);
     testeps_a = max(vec(abs(irfsDSGEeps_a - irfsVarma(:, idx, :))));
     assert(testeps_a < tol);
 end
