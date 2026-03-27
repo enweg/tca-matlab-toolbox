e671f257a3ea9a2d06eb289e4a826707deae9f0b
Enrico Wegner <wegner1996@gmail.com>
2026-03-27 13:58:03 +0100

    adding new test cases that we want to address

--- tests/transmissionTest.m
+++ tests/transmissionTest.m
@@ -216,4 +216,96 @@ function testTransmission7(testCase)
     assert(all(max(abs(effect(4:end) - effectIrfs(4:end)), [], 'all') < sqrt(eps())));
 end
 
+function testTransmission8(testCase)
+    B = jsondecode(fileread('./tests/simulated-svar-k3-p1/B.json'))';
+    Omega = jsondecode(fileread('./tests/simulated-svar-k3-p1/Omega.json'))';
+    irfs = jsondecode(fileread('./tests/simulated-svar-k3-p1/irfs.json'))';
+    irfsOrtho = jsondecode(fileread('./tests/simulated-svar-k3-p1/irfs_ortho.json'))';
 
+    % If the condition is (x1 AND x2), then the effect on x1 should be zero 
+    % because the path does not include x2 yet, but the effect on x2 can 
+    % be anything because x2 is on the path. 
+
+    cond = makeCondition("x1 & x2");
+    effect = transmission(1, B, Omega, cond, "BOmega"); 
+    effectIrfs = transmission(1, irfs, irfsOrtho, cond, "irf");
+
+    % Doing is manually
+    BTilde = B; 
+    OmegaTilde = Omega; 
+    OmegaTilde(2:end, :) = 0; 
+    BTilde(3:end, 1) = 0; 
+
+    manualBOmega = (eye(size(B)) - BTilde) \ OmegaTilde; 
+    manualBOmega = manualBOmega(:, 1);
+    manualBOmega(1, 1) = 0;   % Because x2 is not on the path and user might interpret it.
+
+    manualIrfs = irfs(1, 1) * irfsOrtho(2,1) / irfsOrtho(1,1) * irfsOrtho(:, 2) / irfsOrtho(2, 2);
+
+    assert(all(max(abs(effect - manualBOmega), [], 'all') < sqrt(eps())));
+    assert(all(max(abs(effectIrfs - manualIrfs), [], 'all') < sqrt(eps())));
+    assert(all(max(abs(effect - effectIrfs), [], 'all') < sqrt(eps())));
+
+end
+
+function testTransmission9(testCase)
+    B = jsondecode(fileread('./tests/simulated-svar-k3-p1/B.json'))';
+    Omega = jsondecode(fileread('./tests/simulated-svar-k3-p1/Omega.json'))';
+    irfs = jsondecode(fileread('./tests/simulated-svar-k3-p1/irfs.json'))';
+    irfsOrtho = jsondecode(fileread('./tests/simulated-svar-k3-p1/irfs_ortho.json'))';
+
+    % If the condition is (x1 AND x2) OR x3, then it makes not much sense 
+    % to interpret the effect on x1 and x2 even though the effect on x2 could 
+    % be nonzero. Thus, to prevent the user from making mistakes, we should 
+    % simply set these to zero. However, what does make sense is to interpret 
+    % the effect on x3, since x3 is on the path if it is the outcome variable. 
+    % Thus, the user might want to provide the outcome as the last variable that 
+    % must lie on the path which means we shouldn't just set this value to zero 
+    % to protect the user. 
+
+    cond = makeCondition("(x1 & x2) | x3"); 
+    effect = transmission(1, B, Omega, cond, "BOmega"); 
+    effectIrfs = transmission(1, irfs, irfsOrtho, cond, "irf");
+
+    % Doing it manually. Q((x1 & x2) | x3) = Q(x1 & x2) + Q(x3) - Q(x1 & x2 & x3)
+    % Term 1: Q(x1 & x2)
+    BTilde = B; 
+    OmegaTilde = Omega; 
+    OmegaTilde(2:end, :) = 0; 
+    BTilde(3:end, 1) = 0; 
+    manualBOmega1 = (eye(size(B)) - BTilde) \ OmegaTilde; 
+    manualBOmega1 = manualBOmega1(:, 1); 
+    manualBOmega1(1, 1) = 0;  % To prevent misinterpretation
+
+    manualIrfs1 = irfs(1, 1) * irfsOrtho(2, 1) / irfsOrtho(1, 1) * irfsOrtho(:, 2) / irfsOrtho(2, 2); 
+
+    % Term 2: Q(x3)
+    BTilde = B; 
+    OmegaTilde = Omega; 
+    OmegaTilde(4:end, :) = 0; 
+    BTilde(4:end, 1:2) = 0; 
+    manualBOmega2 = (eye(size(B)) - BTilde) \ OmegaTilde; 
+    manualBOmega2 = manualBOmega2(:, 1); 
+
+    manualIrfs2 = irfs(3, 1) * irfsOrtho(:, 3) / irfsOrtho(3, 3); 
+
+    % Term 3: Q(x1 & x2 & x3)
+    BTilde = B; 
+    OmegaTilde = Omega; 
+    OmegaTilde(2:end, :) = 0; 
+    BTilde(3:end, 1) = 0; 
+    BTilde(4:end, 2) = 0; 
+    manualBOmega3 = (eye(size(B)) - BTilde) \ OmegaTilde; 
+    manualBOmega3 = manualBOmega3(:, 1); 
+    manualBOmega3(1:2, 1) = 0;  % To prevent misinterpretation
+
+    manualIrfs3 = irfs(1, 1) * irfsOrtho(2, 1) / irfsOrtho(1, 1) * ...
+        irfsOrtho(3, 2) / irfsOrtho(2, 2) * irfsOrtho(:, 3) / irfsOrtho(3, 3);
+
+    manualBOmega = manualBOmega1 + manualBOmega2 - manualBOmega3;
+    manualIrfs = manualIrfs1 + manualIrfs2 - manualIrfs3;
+
+    assert(all(max(abs(effect - manualBOmega), [], 'all') < sqrt(eps())));
+    assert(all(max(abs(effectIrfs - manualIrfs), [], 'all') < sqrt(eps())));
+    assert(all(max(abs(effect - effectIrfs), [], 'all') < sqrt(eps())));
+end
